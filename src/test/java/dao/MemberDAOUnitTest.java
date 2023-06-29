9a3d48b6a525593d0e1a552b5ab06a7bd855e8c8
nimxxs <tnals2199@naver.com>
2023-06-29 12:08:49 +0900

    MemberDAO 단위테스트 추가

---
+++ src/test/java/dao/MemberDAOUnitTest.java
@@ -0,0 +1,31 @@
+package dao;
+
+import nimxxs.hello.spring4.dao.MemberDAO;
+import nimxxs.hello.spring4.model.Member;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/servlet-context.xml",
+        "classpath:spring/root-context.xml"})
+@WebAppConfiguration
+public class MemberDAOUnitTest {
+
+    // 테스트 할 대상 객체를 주입받음
+    @Autowired MemberDAO mdao;
+
+    @Test
+    @Transactional  // 데이터 입력 후 자동 삭제
+    public void insertMember() throws Exception {
+        Member m = new Member(null, "abc123a","987xyz","빵빵이","abc123@987xyz.co.kr",null);
+        // assertEquals(테스트메서드, 검증값)
+        assertEquals(mdao.insertMember(m), 1);
+    }
+}
