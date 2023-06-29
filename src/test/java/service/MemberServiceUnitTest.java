7763c7ddb1f399323f6e4e56f28144b0a6236ac9
nimxxs <tnals2199@naver.com>
2023-06-29 12:09:19 +0900

    MemberService 단위테스트 추가

---
+++ src/test/java/service/MemberServiceUnitTest.java
@@ -0,0 +1,32 @@
+package service;
+
+import nimxxs.hello.spring4.dao.MemberDAO;
+import nimxxs.hello.spring4.model.Member;
+import nimxxs.hello.spring4.service.MemberService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/servlet-context.xml",
+        "classpath:spring/root-context.xml"})
+@WebAppConfiguration
+public class MemberServiceUnitTest {
+
+    // 테스트 할 대상 객체를 주입받음
+    @Autowired MemberService msrv;
+
+    @Test
+    @Transactional
+    public void saveMember() throws Exception {
+        Member m = new Member(null, "abc123a","987xyz","빵빵이","abc123@987xyz.co.kr",null);
+        // assertEquals(테스트메서드, 검증값)
+        assertEquals(msrv.saveMember(m), true);
+    }
+}
