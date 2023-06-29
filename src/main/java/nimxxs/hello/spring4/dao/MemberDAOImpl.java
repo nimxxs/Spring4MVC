e73932f6734080c49130fa7d1c29dd622bf405f4
nimxxs <tnals2199@naver.com>
2023-06-29 15:16:13 +0900

    MemberDAO loginMember 구현

--- src/main/java/nimxxs/hello/spring4/dao/MemberDAOImpl.java
+++ src/main/java/nimxxs/hello/spring4/dao/MemberDAOImpl.java
@@ -4,13 +4,18 @@ import nimxxs.hello.spring4.model.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Repository("mdao")
 public class MemberDAOImpl implements nimxxs.hello.spring4.dao.MemberDAO {
 
     // sql.properties에 작성한 sql 불러오기
     @Value("#{sql['insertMember']}") private String insertSQL;
+    @Value("#{sql['loginMember']}") private String loginSQL;
 
     @Autowired
     JdbcTemplate jdbcTemplate;
@@ -24,5 +29,28 @@ public class MemberDAOImpl implements nimxxs.hello.spring4.dao.MemberDAO {
 
         // 쿼리 실행 : update(sql문, 매개변수)
         return jdbcTemplate.update(insertSQL, params);
+    }
+
+    public Member loginMember(Member m) {
+        // 매개변수 정의
+        Object[] params = new Object[] {
+                m.getUserid(), m.getPasswd()
+        };
+
+        // 로그인 매퍼 선언 - 콜백 함수
+        RowMapper<Member> mapper = new LoginMapper();   // 실행 하는 코드가 아님 알려만 주는 코드이다?
+
+        // 쿼리 실행 : query(sql문, 매개변수, 매퍼) - 단일값 반환
+        return jdbcTemplate.queryForObject(loginSQL, params, mapper);
+    }
+
+    private class LoginMapper implements RowMapper<Member> {
+        @Override
+        public Member mapRow(ResultSet rs, int num) throws SQLException {
+            Member m = new Member();
+            m.setUserid(rs.getString(1));
+            m.setName(rs.getString(2));
+            return m;
+        }
     }
 }
