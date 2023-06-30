62bbe7d592202f95f3516b5c399741f3f73d7a9b
nimxxs <tnals2199@naver.com>
2023-06-30 16:53:28 +0900

    BoardService : readBoard 구현

---
+++ src/main/java/nimxxs/hello/spring4/service/BoardService.java
@@ -0,0 +1,10 @@
+package nimxxs.hello.spring4.service;
+
+import nimxxs.hello.spring4.model.Board;
+
+import java.util.List;
+
+public interface BoardService {
+
+    List<Board> readBoard(int cpg);
+}
