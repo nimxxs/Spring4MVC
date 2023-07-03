dde7821c598c5a3b32445b19efee0795326c7fd7
nimxxs <tnals2199@naver.com>
2023-07-03 14:52:17 +0900

    boardDAO : insertBoard 단위테스트

--- src/test/java/dao/BoardDAOUnitTest.java
+++ src/test/java/dao/BoardDAOUnitTest.java
@@ -1,9 +1,7 @@
 package dao;
 
 import nimxxs.hello.spring4.dao.BoardDAO;
-import nimxxs.hello.spring4.dao.MemberDAO;
 import nimxxs.hello.spring4.model.Board;
-import nimxxs.hello.spring4.model.Member;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +19,7 @@ import static org.junit.Assert.assertNotNull;
 @ContextConfiguration({"classpath:spring/servlet-context.xml",
         "classpath:spring/root-context.xml"})
 @WebAppConfiguration
-public class BoardrDAOUnitTest {
+public class BoardDAOUnitTest {
 
     @Autowired BoardDAO bdao;
     @Test
@@ -40,5 +38,13 @@ public class BoardrDAOUnitTest {
 
         assertNotNull(results);
         System.out.println(results);
+    }
+
+    @Test
+    @Transactional
+    public void insertBoard() throws Exception {
+        Board bd = new Board(null,"테스트","abc123", null, null, "냉무");
+
+        assertEquals(bdao.insertBoard(bd),1);
     }
 }
