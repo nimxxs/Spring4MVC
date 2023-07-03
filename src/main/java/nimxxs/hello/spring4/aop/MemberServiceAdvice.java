5068046a010f4179bf694cb5c9d37eba9db99211
nimxxs <tnals2199@naver.com>
2023-07-03 17:00:13 +0900

    join에 대해 포인트컷, 어드바이스 추가

--- src/main/java/nimxxs/hello/spring4/aop/MemberServiceAdvice.java
+++ src/main/java/nimxxs/hello/spring4/aop/MemberServiceAdvice.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpSession;
@@ -16,7 +17,15 @@ public class MemberServiceAdvice {
 
     private Logger logger = LogManager.getLogger(MemberServiceAdvice.class);
 
-    @Around("execution(* nimxxs.hello.spring4.controller.MemberController.myinfo(..))") // 매개변수는 상관없다 = ..
+    @Pointcut("execution(* nimxxs.hello.spring4.controller.MemberController.myinfo(..))")
+    public void myInfoPoint() {}
+
+    @Pointcut("execution(* nimxxs.hello.spring4.controller.MemberController.join(..))")
+    public void joinPoint() {}
+
+    // 매개변수는 상관없다 = ..
+
+    @Around("myInfoPoint()")
     public Object myinfoAOPProcess(ProceedingJoinPoint pjp)
                                             throws Throwable {
         logger.info("myinfoAOPProcess 호출!!");
@@ -37,4 +46,25 @@ public class MemberServiceAdvice {
         return obj;
     }
 
+    @Around("joinPoint()")
+    public Object joinAOPProcess(ProceedingJoinPoint pjp)
+            throws Throwable {
+        logger.info("joinAOPProcess 호출!!");
+        HttpSession sess = null;
+
+        for(Object o : pjp.getArgs()) {
+            if(o instanceof HttpSession)
+                sess = (HttpSession) o;
+        }
+
+        // 포인트컷 대상 메서드 실행
+        // 로그인 안 했다면 -> join으로 이동
+        // 로그인 했다면 -> myinfo로 이동
+        if (sess.getAttribute("member") != null)
+            return "redirect:/member/myinfo";
+
+        //
+        Object obj = pjp.proceed();
+        return obj;
+    }
 }
