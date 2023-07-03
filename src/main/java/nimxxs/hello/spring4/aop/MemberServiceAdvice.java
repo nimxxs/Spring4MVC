7f2f9d1ff7aeedf7d5eb1bfcb044056febc460a2
nimxxs <tnals2199@naver.com>
2023-07-03 16:12:45 +0900

    myinfo에 대해 포인트컷, 어드바이스추가

---
+++ src/main/java/nimxxs/hello/spring4/aop/MemberServiceAdvice.java
@@ -0,0 +1,40 @@
+package nimxxs.hello.spring4.aop;
+
+import nimxxs.hello.spring4.controller.BoardController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpSession;
+
+@Aspect
+@Component
+public class MemberServiceAdvice {
+
+    private Logger logger = LogManager.getLogger(MemberServiceAdvice.class);
+
+    @Around("execution(* nimxxs.hello.spring4.controller.MemberController.myinfo(..))") // 매개변수는 상관없다 = ..
+    public Object myinfoAOPProcess(ProceedingJoinPoint pjp)
+                                            throws Throwable {
+        logger.info("myinfoAOPProcess 호출!!");
+        HttpSession sess = null;
+
+        // join point에서 넘겨준 매개변수에서 원하는 객체 추출
+        for(Object o : pjp.getArgs()) {
+            if(o instanceof HttpSession)
+                sess = (HttpSession) o;
+        }
+
+        // 포인트컷 대상 메서드 실행
+        if (sess.getAttribute("member") == null)
+            return "redirect:/member/login";
+
+        //
+        Object obj = pjp.proceed();
+        return obj;
+    }
+
+}
